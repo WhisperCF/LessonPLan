d7baf1e45a1ac01c09f13c8b14c080c7afade97b
Chris Fouts <cbfouts@micromat.com>
2022-01-31 09:08:35 -0800

    Initial Commit

---
+++ Planify/PlanifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanifyApp.swift
+//  Planify
+//
+//  Created by Christopher Fouts on 1/31/22.
+//
+
+import SwiftUI
+
+@main
+struct PlanifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
